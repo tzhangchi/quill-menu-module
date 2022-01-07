3b942b8c09d0f44977e4424b983af45c27830d46
zhangc <zhangchi3@outlook.com>
2022-01-08 00:17:37 +0800

    feat:build ui

--- src/modules/Menu.ts
+++ src/modules/Menu.ts
@@ -3,6 +3,12 @@ const Module = Quill.import('core/module')
 const KEYS = {
   SLASH: 191
 }
+const BLOCK_MENUS = [
+  { name: 'H1', desc: 'Just Writing Desc' },
+  { name: 'H2', desc: 'Just Writing Desc' },
+  { name: 'H3', desc: 'Just Writing Desc' },
+  { name: 'Code', desc: 'Just Writing Desc' }
+]
 class Menu extends Module {
   constructor(quill: Quill, options: any) {
     super(quill, options)
@@ -15,9 +21,61 @@ class Menu extends Module {
   onShowMenu() {
     console.log('onShowMenu')
     this.isShowBlockMenus_ = true
+    this.buildMenu()
+  }
+  buildMenu() {
+    let container = document.createElement('div')
+    container.id = 'quill-menu-module'
+    let html = `<div
+    class="fixed top-14 left-4 shadow-lg border w-64 z-50 bg-white"
+  >
+    <p class="text-gray-500 p-2 border-b border-b-gray-200 text-sm">
+      BASICK BLOCKS
+    </p>
+    <ul>`
+
+    BLOCK_MENUS.forEach(item => {
+      html += `<li
+        class="
+          px-4
+          py-2
+          flex
+          h-16
+          border-b border-b-gray-200
+          hover:bg-gray-200
+          cursor-pointer
+        "
+        @click="onClickMenu(item)"
+      >
+        <div class="flex">
+          <div
+            class="
+              w-12
+              h-12
+              text-center
+              leading-10
+              border border-gray-200
+              rounded-lg
+            "
+          >
+          ${item.name}
+          </div>
+        </div>
+        <div class="flex pl-2 flex-col">
+          <div class="text-black flex text-sm">${item.name}</div>
+          <div class="text-gray-400 flex text-sm">${item.desc}</div>
+        </div>
+      </li>
+      `
+    })
+
+    html += `</ul>`
+    container.innerHTML = html
+    document.body.appendChild(container)
   }
   hideMenu() {
     this.isShowBlockMenus = false
+    document.getElementById('quill-menu-module')?.remove()
     console.log('onHideMenu')
   }
   onDocumentClick() {
